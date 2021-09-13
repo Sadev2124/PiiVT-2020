7506595b907eed8667ab6fe94f020d05c6c62721
Sanja Golubovic <sanja.golubovic.17@singimail.rs>
2021-09-13 12:41:25 +0200

    Implemented the database connection and used it in the subject service

--- 03-back-end/src/components/subject/router.ts
+++ 03-back-end/src/components/subject/router.ts
@@ -1,10 +1,11 @@
 import * as express from "express";
 import SubjectService from './service';
 import SubjectController from "./controller";
+import IApplicationResources from '../../common/IApplicationResources.interface';
 
 export default class SubjectRouter {
-    public static  setupRoutes(application: express.Application) {
-        const subjectService: SubjectService = new SubjectService();
+    public static  setupRoutes(application: express.Application, resources: IApplicationResources) {
+        const subjectService: SubjectService = new SubjectService(resources.databaseConnection);
         const subjectController: SubjectController = new SubjectController(subjectService);
 
         application.get("/subject", subjectController.getAll.bind(subjectController));
--- 03-back-end/src/components/subject/service.ts
+++ 03-back-end/src/components/subject/service.ts
@@ -1,40 +1,51 @@
 import SubjectModel from "./model";
+import * as mysql2 from 'mysql2/promise';
+import { isArrayBufferView } from "util/types";
 
 class SubjectService{
+    private db: mysql2.Connection;
+
+    constructor(db: mysql2.Connection) {
+        this.db = db;
+    }
+
+    protected async adaptModel(row: any): Promise<SubjectModel>{
+        const item: SubjectModel = new SubjectModel();
+
+        item.subjectId = row?.subject_id;
+        item.name = row?.name;
+
+        return item;
+    }
+
     public async getAll(): Promise<SubjectModel[]> {
         const lista: SubjectModel[] = [];
 
-        lista.push({
-            subjectId: 1,
-            name: "Matematika"
-        });
+        const sql: string = "SELECT * FROM subject;";
+        const [ rows, columns ] = await this.db.execute(sql);
 
-        lista.push({
-            subjectId: 2,
-            name: "Srpski"
-        });
+        if (Array.isArray(rows)) {
+            for (const row of rows) {
+                lista.push(await this.adaptModel(row));
+            }
+        }
 
         return lista;
     }
 
     public async getById(subjectId: number): Promise<SubjectModel|null> {
-        if (subjectId === 1 || subjectId === 2) {
-            if (subjectId === 1) {
-                return {
-                    subjectId: 1,
-                    name: "Matematika"
-                };
-            }
+        const sql: string = "SELECT * FROM subject WHERE subject_id = ?;";
+        const [ rows, columns ] = await this.db.execute(sql, [subjectId]);
 
-            if (subjectId === 2) {
-                return {
-                    subjectId: 2,
-                    name: "Srpski"
-                };
-            } 
-        } else {
+        if (!Array.isArray(rows)) {
             return null;
         }
+
+        if (rows.length === 0) {
+            return null;
+        }
+
+        return await this.adaptModel(rows[0]);
     }
 }
 
